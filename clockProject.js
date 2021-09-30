954f3739ffa5d6c7368f40a868f4f714dffde14d
paritosh251197 <paritoshkashyap25@gmail.com>
2021-09-30 13:52:49 +0530

    changes done

--- clockProject.js
+++ clockProject.js
@@ -7,6 +7,7 @@ const greet = document.getElementById("greetings");
 const party = document.getElementById('party');
 const mess = document.getElementById('textInHavingLunch');
 const lunchimg = document.getElementById('having_lunch');
+// const wish = document.getElementById("greetings");
 
 function concatZero(value) {
     return value < 10 ? '0'.concat(value) : value
@@ -32,92 +33,87 @@ setInterval(time_Update, 1000);
 // clock section end
 
 
+const tarik = new Date();
+const sec = tarik.getSeconds();
+const min = tarik.getMinutes();
+const hr = tarik.getHours();
 
-function realTimeUpdate() {
-    const tarik = new Date();
-    const sec = tarik.getSeconds();
-    const min = tarik.getMinutes();
-    const hr = tarik.getHours();
-    if (hr >= 5 && hr < 12) {
-        wakeUp();
-    }
-    else if (hr >= 12 && hr < 18) {
-        haveLunch();
-    }
-    else if (hr >= 20 && hr < 24) {
-        sleep();
-    }
-    else {
-        greet.innerText = "Have a Good Day";
-        mess.innerText = "Chill Karo";
-        lunchimg.style.backgroundImage = "url('./chill.svg')";
-    }
-}
 
-realTimeUpdate();
+// default function
+function chillkaro() {
+    mess.innerText = "Chill Karo";
+    lunchimg.style.backgroundImage = "url('./chill.svg')";
+    greet.style.display = "none";
+};
+chillkaro();
 
 
 
-// party button
-// const partybtn = document.querySelector(".party-time");
+// party function
 let isParty = false;
 party.addEventListener("click", () => {
     if (isParty) {
         party.innerHTML = "Lets Party";
-        realTimeUpdate();
+        greet.innerText = "";
+        mess.innerText = "Chill Karo";
+        lunchimg.style.backgroundImage = "url('./chill.svg')";
+        greet.style.display = "none";
     } else {
         party.innerHTML = "End Party";
         lunchimg.style.backgroundImage = "url('./party2.svg')";
-        // lunchimg.style.backgroundPositionY = "3px";
         greet.innerHTML = "Enjoy The Party";
-        mess.innerHTML = "Humari Paawri Ohh Raii aae";
+        mess.innerHTML = "Humari Paawri Ohh Rai aae";
+        greet.style.display = "block";
     }
-    isParty =! isParty; 
+    isParty = !isParty;
 });
 
 
+
+
+// select time function
 document.querySelector("#wake-up-timespan").addEventListener("change", function () {
-    if(this.value === "select"){
-        console.log(this.value);
-        realTimeUpdate();
-    } else {
+    if (this.value == hr) {
         wakeUp();
+    } else {
+        chillkaro();
     }
 });
- 
-document.querySelector("#lunch-timespan").addEventListener("change", function () { 
-    if(this.value === "select"){
-        console.log(this.value);
-        realTimeUpdate();
-    } else {
+
+document.querySelector("#lunch-timespan").addEventListener("change", function () {
+    if (this.value == hr) {
         haveLunch();
+    } else {
+        chillkaro();
     }
 });
 
 document.querySelector("#napTime").addEventListener("change", function () {
-    if(this.value === "select"){
-        console.log(this.value);
-        realTimeUpdate();
-    } else {
+    if (this.value == hr) {
         sleep();
-    }  
-}); 
+    } else {
+        chillkaro();
+    }
+});
 
 
-function wakeUp () {
+function wakeUp() {
     greet.innerHTML = "Good Morning !!";
     mess.innerText = "Wake up"
     lunchimg.style.backgroundImage = "url('./Sunny day-bro.svg')";
+    greet.style.display = "block";
 }
-function haveLunch () {
+function haveLunch() {
     greet.innerHTML = "Good AfterNoon !!";
     mess.innerText = 'Lets Have Some Lunch !!'
     lunchimg.style.backgroundImage = "url('Pizza sharing-cuate.svg')";
+    greet.style.display = "block";
 }
-function sleep () {
+function sleep() {
     greet.innerHTML = "Good Night Dear !!";
     mess.innerText = 'Nap Time !!';
     lunchimg.style.backgroundImage = "url('./Sleep analysis-cuate.svg')";
+    greet.style.display = "block";
 }
 
 
@@ -126,4 +122,38 @@ function sleep () {
 
 
 
+// kachra below
+// function realTimeUpdate() {
+//     if (hr >= 5 && hr < 12) {
+//         wakeUp();
+//     }
+//     else if (hr >= 12 && hr < 18) {
+//         haveLunch();
+//     }
+//     else if (hr >= 20 && hr < 24) {
+//         sleep();
+//     }
+//     else {
+//         greet.innerText = "Have a Good Day";
+//         mess.innerText = "Chill Karo";
+//         lunchimg.style.backgroundImage = "url('./chill.svg')";
+//     }
+// }
 
+// realTimeUpdate();
+
+
+
+
+// let ids = document.querySelector("#AM11").value;
+// console.log(ids);
+// console.log(hr);
+// console.log(time_Update);
+
+
+// if(this.value === "select"){
+    //     console.log(this.value);
+    //     realTimeUpdate();
+    // } else {
+        //     wakeUp();
+        // }
